293c621d4af24d9316824e28cdcc27d9c9fdf5db
gpi <gpimpale29@gmail.com>
2024-09-30 21:38:34 +0100

    reduce number of copies

--- src/render_system/interactive_rendering.rs
+++ src/render_system/interactive_rendering.rs
@@ -156,7 +156,7 @@ fn create_swapchain(
         device.clone(),
         surface.clone(),
         SwapchainCreateInfo {
-            min_image_count: 8,
+            min_image_count: 3,
             image_format: Format::B8G8R8A8_SRGB,
             image_extent: window.inner_size().into(),
             image_usage: ImageUsage::TRANSFER_DST,
